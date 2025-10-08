5c0ba621614759105513cf5fd585d2878b160426
lorsmunka <112896149+lorsmunka@users.noreply.github.com>
2025-10-08 19:27:11 +0200

    mock data generation

--- distillation/ModelHandler.py
+++ distillation/ModelHandler.py
@@ -1,6 +1,18 @@
+from time import time
+
+
 class ModelHandler:
     def __init__(self):
         print("ModelHandler initialized")
 
-    def generate_example(self, sentence):
-        return f"Example generated for: {sentence}"
+    def generate_training_example(self, sentence):
+        start_time = time()
+
+        elapsed_time = time() - start_time
+        print(
+            f"Generated training example -> took {elapsed_time:.2f} seconds.")
+
+        return {
+            "sentence": sentence,
+            "generated_text": f"Generated text for: {sentence}"
+        }
--- distillation/main.py
+++ distillation/main.py
@@ -1,5 +1,12 @@
 from SentenceHandler import SentenceHandler
 from TelemetryHandler import TelemetryHandler
+from ModelHandler import ModelHandler
 
 sentenceHandler = SentenceHandler()
 telemetryHandler = TelemetryHandler()
+modelHandler = ModelHandler()
+
+for i in range(sentenceHandler.sentence_count):
+    sentence = sentenceHandler.get_sentence(i)
+    training_example = modelHandler.generate_training_example(sentence)
+    telemetryHandler.processed_sentence_count += 1
