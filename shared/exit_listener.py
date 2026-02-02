e9399f05d96ab80374d0cb8856ae02c5d36fe7db
lorsmunka <112896149+lorsmunka@users.noreply.github.com>
2026-02-02 17:47:18 +0100

    headles gracefull stop fix

--- shared/exit_listener.py
+++ shared/exit_listener.py
@@ -1,12 +1,35 @@
-from pynput import keyboard
+import signal
+import sys
+
+try:
+    from pynput import keyboard
+    PYNPUT_AVAILABLE = True
+except ImportError:
+    PYNPUT_AVAILABLE = False
 
 
 class ExitListener:
     def __init__(self, exit_key='%'):
         self.should_exit = False
         self.exit_key = exit_key
-        self.listener = keyboard.Listener(on_press=self._on_press)
-        self.listener.start()
+        self.listener = None
+
+        if PYNPUT_AVAILABLE:
+            try:
+                self.listener = keyboard.Listener(on_press=self._on_press)
+                self.listener.start()
+            except Exception:
+                self._setup_signal_handler()
+        else:
+            self._setup_signal_handler()
+
+    def _setup_signal_handler(self):
+        signal.signal(signal.SIGINT, self._signal_handler)
+        print("Headless mode: press Ctrl+C for graceful exit")
+
+    def _signal_handler(self, signum, frame):
+        self.should_exit = True
+        print("\nExit signal registered.")
 
     def _on_press(self, key):
         try:
@@ -20,4 +43,5 @@ class ExitListener:
         return self.should_exit
 
     def stop(self):
-        self.listener.stop()
+        if self.listener:
+            self.listener.stop()
